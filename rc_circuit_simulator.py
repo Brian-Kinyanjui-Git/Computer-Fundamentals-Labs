f718f1da288d7708c8c61724cdda645f7b01590d
Brian-Kinyanjui-Git <2007njorogebrian@gmail.com>
2026-02-02 19:02:48 +0300

    Update rc_circuit_simulator.py

--- rc_circuit_simulator.py
+++ rc_circuit_simulator.py
@@ -1,18 +1,12 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
-
-
 R = 1000          
 C = 0.000001      
 V_initial = 5     
 
-
 time = np.linspace(0, 0.01, 1000)
 Vc = V_initial * np.exp(-time / (R * C))
-
 plt.plot(time, Vc)
-
 plt.title("RC Circuit Discharging Curve")
 plt.xlabel("Time (seconds)")
 plt.ylabel("Voltage (Volts)")
